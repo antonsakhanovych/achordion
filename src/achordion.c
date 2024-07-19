425322db12ea2a814d493a4df6c28098a001607e
Anton Sakhanovych <antonsakhanovych@gmail.com>
2024-07-18 21:33:35 -0400

    add mutex for ring_buffer

--- src/achordion.c
+++ src/achordion.c
@@ -1,13 +1,23 @@
+#include <errno.h>
+#include <pthread.h>
 #include <raylib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
 #define NUM_CHANNELS 1
 #define SAMPLE_RATE 48000
 #define BUFFER_SIZE_FRAMES 2048
 
-#define MINIAUDIO_IMPLEMENTATION
-#include "miniaudio.h"
+typedef struct {
+    ma_pcm_rb ring_buffer;
+    pthread_mutex_t mutex;
+} AudioData;
+
+int audio_data_init(AudioData* audio_data);
 
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
@@ -16,32 +26,39 @@ void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uin
         fprintf(stderr, "ERROR: Format should be ma_format_f32\n");
         exit(1);
     }
-    // Acquire the ring buffer
-    ma_pcm_rb* ring_buf = (ma_pcm_rb*)pDevice->pUserData;
+    // Acquire the AudioData struct
+    AudioData* audio_data = (AudioData *)pDevice->pUserData;
     // Writeable buffer that will be available after calling ma_pcm_rb_acquire_write
     float* buffer_to_write;
+    // Acquire mutex lock
+    if(pthread_mutex_lock(&audio_data->mutex) == EOWNERDEAD) {
+        fprintf(stderr, "ERROR: Owner thread that locked the mutex is dead\n");
+        return;
+    }
     // NOTE: frameCount is updated to the amount of frames allowed to write
-    ma_pcm_rb_acquire_write(ring_buf, &frameCount, (void**)&buffer_to_write);
+    ma_pcm_rb_acquire_write(&audio_data->ring_buffer, &frameCount, (void**)&buffer_to_write);
     // Here write to the buffer
     memcpy(buffer_to_write, pInput, sizeof(float) * frameCount);
-    ma_pcm_rb_commit_write(ring_buf, frameCount);
+    // Commit the changes to the ring buffer
+    ma_pcm_rb_commit_write(&audio_data->ring_buffer, frameCount);
+    // Unlock the mutex
+    pthread_mutex_unlock(&audio_data->mutex);
 }
 
 int main(int argc, char** argv)
 {
-    ma_pcm_rb ring_buf;
-    ma_device_config device_config = ma_device_config_init(ma_device_type_capture);
     ma_result result;
+    AudioData audio_data = {0};
+    ma_device_config device_config = ma_device_config_init(ma_device_type_capture);
     ma_device device;
     device_config.capture.format = ma_format_f32;
     device_config.capture.channels = NUM_CHANNELS;
     device_config.sampleRate = SAMPLE_RATE;
     device_config.dataCallback = data_callback;
-    device_config.pUserData = &ring_buf;
+    device_config.pUserData = &audio_data;
 
-    result = ma_pcm_rb_init(ma_format_f32, NUM_CHANNELS, BUFFER_SIZE_FRAMES, NULL, NULL, &ring_buf);
-    if(result != MA_SUCCESS) {
-        fprintf(stderr, "ERROR: Failed to initialize ring buffer.");
+    if(audio_data_init(&audio_data) < 0) {
+        fprintf(stderr, "ERROR: Failed to initialize audio data\n");
         return -1;
     }
 
@@ -76,49 +93,23 @@ int main(int argc, char** argv)
 
     return 0;
 }
-//#define MINIAUDIO_IMPLEMENTATION
-//#include "miniaudio.h"
-//
-//#include <stdio.h>
-//
-//int main(int argc, char** argv)
-//{
-//    ma_result result;
-//    ma_context context;
-//    ma_device_info* pPlaybackDeviceInfos;
-//    ma_uint32 playbackDeviceCount;
-//    ma_device_info* pCaptureDeviceInfos;
-//    ma_uint32 captureDeviceCount;
-//    ma_uint32 iDevice;
-//
-//    if (ma_context_init(NULL, 0, NULL, &context) != MA_SUCCESS) {
-//        printf("Failed to initialize context.\n");
-//        return -2;
-//    }
-//
-//    result = ma_context_get_devices(&context, &pPlaybackDeviceInfos, &playbackDeviceCount, &pCaptureDeviceInfos, &captureDeviceCount);
-//    if (result != MA_SUCCESS) {
-//        printf("Failed to retrieve device information.\n");
-//        return -3;
-//    }
-//
-//    printf("Playback Devices\n");
-//    for (iDevice = 0; iDevice < playbackDeviceCount; ++iDevice) {
-//        printf("    %u: %s\n", iDevice, pPlaybackDeviceInfos[iDevice].name);
-//    }
-//
-//    printf("\n");
-//
-//    printf("Capture Devices\n");
-//    for (iDevice = 0; iDevice < captureDeviceCount; ++iDevice) {
-//        printf("    %u: %s\n", iDevice, pCaptureDeviceInfos[iDevice].name);
-//    }
-//
-//
-//    ma_context_uninit(&context);
-//
-//    (void)argc;
-//    (void)argv;
-//    return 0;
-//}
-//
+
+int audio_data_init(AudioData* audio_data)
+{
+    if(pthread_mutex_init(&audio_data->mutex, NULL) < 0) {
+        fprintf(stderr, "ERROR: Failed to initialize mutex");
+        return -1;
+    }
+
+    ma_result result = ma_pcm_rb_init(ma_format_f32,
+                                      NUM_CHANNELS,
+                                      BUFFER_SIZE_FRAMES,
+                                      NULL,
+                                      NULL,
+                                      &audio_data->ring_buffer);
+    if(result != MA_SUCCESS) {
+        fprintf(stderr, "ERROR: Failed to initialize ring buffer.");
+        return -1;
+    }
+    return 0;
+}
